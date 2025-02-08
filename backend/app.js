16f3c8efb4e2ddd6764682d5dc30f2d98f012428
tanveer128423 <khalidtanveer653@gmail.com>
2025-02-08 10:32:26 +0530

    Updated backend/app.js and dependencies

--- backend/app.js
+++ backend/app.js
@@ -21,3 +21,30 @@ app.get("/error-test", (req, res, next) => {
 app.use(errorMiddleware);
 
 module.exports = app;
+
+
+const express = require("express");
+const ErrorHandler = require("./middleware/error");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+ 
+app.use(express.json());
+app.use(cookieParser());
+app.use("/",express.static("uploads"));
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+// config
+if (process.env.NODE_ENV !== "PRODUCTION") {
+    require("dotenv").config({
+      path: "backend/config/.env",
+    });
+};
+//import Routes
+const user = require("./controller/user");
+ 
+app.use("/api/v2/user", user);
+ 
+// it's for ErrorHandling
+app.use(ErrorHandler);
+ 
+module.exports = app;
+Types:application,errors,router
