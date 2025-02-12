ae7b0567494270df0e0ecdb70fd240ee98c4fed6
tanveer128423 <khalidtanveer653@gmail.com>
2025-02-12 09:51:57 +0530

    Updated backend files and added new utility functions

--- backend/utils/multer.js
+++ backend/utils/multer.js
@@ -1,18 +1,17 @@
 const multer = require('multer');
 
+
 // Configure multer storage
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'uploads/'); // Define your upload folder
-    },
-    filename: function(req, file, cb) {
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-        const filename = file.originalname.split(".")[0];
-        cb(null, filename + "-" + uniqueSuffix + ".png");
-    }
+  destination: '../uploads',
+  filename: function (req, file, cb) {
+    console.log(req.body);
+    const uniqueSuffix = Date.now() + '-' + Math.round.apply(Math.random() * 1e9);
+    // Define a unique filename
+    const filename = file.originalname.split(".")[0];
+    cb(null, filename + "-" + uniqueSuffix + ".png"); // Define
+  },
 });
 
 // Initialize upload object
-const upload = multer({ storage: storage });
-
-module.exports = upload;
+exports.upload = multer({ storage: storage });
